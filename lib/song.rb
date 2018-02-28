58c1aceb050fc7c13563385c5a4b97f4ed72e809
Evan Candler <emcandler21@gmail.com>
2018-02-28 21:26:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,8 +40,8 @@ class Song
     self.all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(filename)
-    artist_split = filename.split(" - ")
+  def self.new_from_filename(file)
+    artist_split = file.split(" - ")
     artist_name = artist.split[0]
     song_split = artist_split[1].split(".")
     song_name = song_split[0]
